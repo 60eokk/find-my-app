1305164bad6a41ae52649d618d1fde4fa6624fb4
Keith Jung <60eokk@gmail.com>
2024-08-15 13:59:40 -0500

    implement maps through react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+
+
+
+
 
 function App() {
+  const [position, setPosition] = React.useState([50, 5]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Get Notified!!</h1>
+      <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}></Marker>
+      </MapContainer>
     </div>
   );
 }
