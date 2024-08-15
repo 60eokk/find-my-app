5909be81ec28de6cb48c6b2ab9f8ded27e6d0b0c
Keith Jung <60eokk@gmail.com>
2024-08-15 14:07:46 -0500

    changes to map UI

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
 
-
-
-
 function App() {
   const [position, setPosition] = React.useState([50, 5]);
   return (
-    <div>
-      <h1>Get Notified!!</h1>
-      <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <div className="container">
+      <h1 className="heading">Get Notified!!</h1>
+      <MapContainer center={position} zoom={13} className="map">
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
@@ -20,6 +17,6 @@ function App() {
       </MapContainer>
     </div>
   );
-}
+};
 
 export default App;
