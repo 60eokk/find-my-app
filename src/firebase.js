afe315b4d465cf4dcd28eb24bb25719bc2d98ffc
Keith Jung <60eokk@gmail.com>
2024-08-20 14:30:37 -0500

    firebase console installation

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDz_ZkxfgkRZCOG94UlVJMkrKkASI2PcRs",
+    authDomain: "find-my-app-107db.firebaseapp.com",
+    projectId: "find-my-app-107db",
+    storageBucket: "find-my-app-107db.appspot.com",
+    messagingSenderId: "115773395980",
+    appId: "1:115773395980:web:b76bc9f91814c8d4d88a29",
+    measurementId: "G-S7HPEK5M5T"
+  };
+
+const app = initializeApp(firebaseConfig); // default from Firebase console
+const analytics = getAnalytics(app); // default from Firebase console
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
