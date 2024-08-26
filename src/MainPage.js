4b5c3ad5c59e30bf03830bd690b90c4380cb5c14
Keith Jung <60eokk@gmail.com>
2024-08-26 12:26:07 -0500

    added map cursor but not showing correctly

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import L from 'leaflet'
 
 const MainPage = () => {
   const [position, setPosition] = React.useState(null);
@@ -9,6 +10,7 @@ const MainPage = () => {
   React.useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
+        console.log('Geolocation success:', pos); // log to check position is correct
         setPosition([pos.coords.latitude, pos.coords.longitude]);
       },
       (error) => {
@@ -19,19 +21,27 @@ const MainPage = () => {
     );
   }, []);
 
+  const customMarkerIcon = new L.Icon({
+    iconUrl: require('./mapcursor.png'),
+    iconSize: [32, 32],
+    iconAnchor: [16, 32], // Point of the icon which will correspond to marker's location
+    popupAnchor: [0, -32] // Point from which the popup should open relative to the iconAnchor
+  });
+
   return (
     <div style={styles.container}>
       <h1 style={styles.heading}>Get Notified!!</h1>
       <div style={styles.mapContainer}>
-        {position && (
+        {position ? (
           <MapContainer center={position} zoom={13} style={styles.map}>
             <TileLayer
               url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=${MapboxToken}`}
               maxZoom={18}
-              errorTileUrl="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" // Fallback to OpenStreetMap
             />
-            <Marker position={position}></Marker>
+            <Marker position={position} icon={customMarkerIcon}></Marker>
           </MapContainer>
+        ) : (
+          <p>Loading map...</p>
         )}
       </div>
     </div>
