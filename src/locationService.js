ec9e56069489f3169da2e9d24eb185674a819dd2
Keith Jung <60eokk@gmail.com>
2024-09-10 09:10:12 -0500

    added locationService.js

---
+++ src/locationService.js
@@ -0,0 +1,15 @@
+import { db } from './firebase';
+import { doc, updateDoc, getDoc } from 'firebase/firestore';
+
+export const updateUserLocation = async (userId, latitude, longitude) => {
+  const userRef = doc(db, "users", userId);
+  await updateDoc(userRef, {
+    location: { latitude, longitude }
+  });
+};
+
+export const getFriendLocation = async (friendId) => {
+  const friendRef = doc(db, "users", friendId);
+  const friendDoc = await getDoc(friendRef);
+  return friendDoc.data()?.location || null;
+};
