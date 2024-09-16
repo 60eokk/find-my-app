1307fb9ed7b2d271785838ffc7c775b00a1e4196
Keith Jung <60eokk@gmail.com>
2024-09-16 16:41:50 -0500

    console log shows error from firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDz_ZkxfgkRZCOG94UlVJMkrKkASI2PcRs",
-    authDomain: "find-my-app-107db.firebaseapp.com",
-    projectId: "find-my-app-107db",
-    storageBucket: "find-my-app-107db.appspot.com",
-    messagingSenderId: "115773395980",
-    appId: "1:115773395980:web:b76bc9f91814c8d4d88a29",
-    measurementId: "G-S7HPEK5M5T"
-  };
+  apiKey: "AIzaSyDz_ZkxfgkRZCOG94UlVJMkrKkASI2PcRs",
+  authDomain: "find-my-app-107db.firebaseapp.com",
+  projectId: "find-my-app-107db",
+  storageBucket: "find-my-app-107db.appspot.com",
+  messagingSenderId: "115773395980",
+  appId: "1:115773395980:web:b76bc9f91814c8d4d88a29",
+  measurementId: "G-S7HPEK5M5T"
+};
 
 const app = initializeApp(firebaseConfig); // default from Firebase console
 const analytics = getAnalytics(app); // default from Firebase console
